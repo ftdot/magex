0adac8bb0cb710a8ad1603f3fdef9da4fd2d95a4
ftdot <ftdoot@gmail.com>
2024-02-12 15:58:54 +0200

    Added colliders component

---
+++ components/colliders/boxcollider.go
@@ -0,0 +1,106 @@
+package colliders
+
+import (
+	"image/color"
+	"math"
+
+	"github.com/ftdot/magex/game"
+	"github.com/ftdot/magex/physics/collision2d"
+	"github.com/ftdot/magex/utils"
+	"github.com/ftdot/magex/utils/interfaces"
+	"github.com/ftdot/magex/utils/mmath"
+	"github.com/ftdot/magex/utils/vector2"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+// Collider that defines a box around the sprite.
+type BoxCollider struct {
+	Sprite         interfaces.ISprite
+	BoxPolygon     *collision2d.Polygon
+	PositionOffset *vector2.Vector2
+	SizeScalar     *vector2.Vector2
+
+	ID string // System variable with ID of the component.
+}
+
+func NewBoxCollider(sprite interfaces.ISprite) *BoxCollider {
+	return &BoxCollider{
+		Sprite:         sprite,
+		BoxPolygon:     collision2d.NewBox(vector2.Null.Copy(), -1, -1).ToPolygon(),
+		PositionOffset: vector2.Null.Copy(),
+		SizeScalar:     vector2.Identity.Copy(),
+		ID:             utils.GenerateComponentID(),
+	}
+}
+
+////
+
+func (bc *BoxCollider) SetSizeScalar(ss *vector2.Vector2) {
+	bc.SizeScalar = ss
+}
+
+func (bc *BoxCollider) GetSizeScalar() *vector2.Vector2 {
+	return bc.SizeScalar
+}
+
+func (bc *BoxCollider) SetPositionOffset(posOffset *vector2.Vector2) {
+	bc.PositionOffset = posOffset
+}
+
+func (bc *BoxCollider) GetPositionOffset() *vector2.Vector2 {
+	return bc.PositionOffset
+}
+
+func (bc *BoxCollider) GetPolygon() *collision2d.Polygon {
+	return bc.BoxPolygon
+}
+
+func (bc *BoxCollider) GetSprite() interfaces.ISprite {
+	return bc.Sprite
+}
+
+////
+
+func (bc *BoxCollider) PhysUpdate(game *game.GameBase) error {
+
+	bounds := bc.Sprite.GetImageBounds().Mul(bc.Sprite.GetTransform().GetScale()).Mul(bc.SizeScalar)
+	pivot := bc.Sprite.GetPivotScaled()
+	pivotOpposite := bc.Sprite.GetPivotOpposite().Mul(bc.Sprite.GetTransform().GetScale())
+
+	boxPol := collision2d.NewBox(bc.Sprite.GetTransform().GetPosition().Add(bc.PositionOffset), bounds.X, bounds.Y).ToPolygon()
+	boxPol.Translate(pivot.X, pivot.Y)
+	boxPol.Rotate(math.Mod(bc.Sprite.GetTransform().GetRotation(), 360) * mmath.RadiansMeasurement90)
+	boxPol.Translate(pivotOpposite.X, pivotOpposite.Y)
+
+	bc.BoxPolygon = boxPol
+
+	return nil
+}
+
+func (bc *BoxCollider) DrawUIPriority() float64 {
+	return 90000
+}
+
+func (bc *BoxCollider) DrawUI(game *game.GameBase, screen *ebiten.Image) {
+
+	verts := bc.BoxPolygon.Points
+	pos := bc.BoxPolygon.Pos
+	for i := 0; i < len(verts); i++ {
+		vert := verts[i].Add(pos)
+		next := verts[0].Add(pos)
+
+		if i < len(verts)-1 {
+			next = verts[i+1].Add(pos)
+		}
+		ebitenutil.DrawLine(screen, vert.X, vert.Y, next.X, next.Y, color.RGBA{25, 255, 25, 255})
+
+	}
+}
+
+////
+
+func (bc *BoxCollider) GetID() string {
+	return bc.ID
+}
---
+++ components/colliders/circlecollider.go
@@ -0,0 +1,101 @@
+package colliders
+
+import (
+	"image/color"
+
+	"github.com/ftdot/magex/game"
+	"github.com/ftdot/magex/physics/collision2d"
+	"github.com/ftdot/magex/utils"
+	"github.com/ftdot/magex/utils/interfaces"
+	"github.com/ftdot/magex/utils/vector2"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+// Collider that defines a circle around the sprite.
+type CircleCollider struct {
+	Sprite         interfaces.ISprite
+	Circle         *collision2d.Circle
+	PositionOffset *vector2.Vector2
+	RadiusScalar   float64
+
+	ID string // System variable with ID of the component.
+}
+
+func NewCircleCollider(sprite interfaces.ISprite) *CircleCollider {
+	return &CircleCollider{
+		Sprite:         sprite,
+		Circle:         collision2d.NewCircle(vector2.Null.Copy(), -1),
+		PositionOffset: vector2.Null.Copy(),
+		RadiusScalar:   1,
+		ID:             utils.GenerateComponentID(),
+	}
+}
+
+////
+
+func (cc *CircleCollider) SetRadiusScalar(rs float64) {
+	cc.RadiusScalar = rs
+}
+
+func (cc *CircleCollider) GetRadiusScalar() float64 {
+	return cc.RadiusScalar
+}
+
+func (cc *CircleCollider) SetPositionOffset(posOffset *vector2.Vector2) {
+	cc.PositionOffset = posOffset
+}
+
+func (cc *CircleCollider) GetPositionOffset() *vector2.Vector2 {
+	return cc.PositionOffset
+}
+
+func (cc *CircleCollider) GetPolygon() *collision2d.Polygon {
+	return cc.Circle.ToPolygon()
+}
+
+func (cc *CircleCollider) GetSprite() interfaces.ISprite {
+	return cc.Sprite
+}
+
+////
+
+func (cc *CircleCollider) PhysUpdate(game *game.GameBase) error {
+	cc.Circle = collision2d.NewCircle(
+		cc.Sprite.GetTransform().GetPosition().Add(cc.Sprite.GetPivotOppositeScaled()).Add(cc.PositionOffset),
+		cc.Sprite.GetImageBounds().X*cc.Sprite.GetTransform().GetScale().X/2*cc.RadiusScalar,
+	)
+	return nil
+}
+
+func (cc *CircleCollider) DrawUIPriority() float64 {
+	return 90000
+}
+
+// func (cc CircleCollider) DrawQueued(game *game.MagexGame, screen *ebiten.Image) {
+// 	c := cc.Circle.Get()
+// 	ebitenutil.DrawCircle(screen, c.Pos.X, c.Pos.Y, c.R, color.RGBA{25, 255, 25, 90})
+// }
+
+func (cc *CircleCollider) DrawUI(game *game.GameBase, screen *ebiten.Image) {
+
+	verts := cc.Circle.ToPolygon().Points
+	pos := cc.Circle.Pos
+	for i := 0; i < len(verts); i++ {
+		vert := verts[i].Add(pos)
+		next := verts[0].Add(pos)
+
+		if i < len(verts)-1 {
+			next = verts[i+1].Add(pos)
+		}
+		ebitenutil.DrawLine(screen, vert.X, vert.Y, next.X, next.Y, color.RGBA{25, 25, 255, 255})
+
+	}
+}
+
+////
+
+func (bc *CircleCollider) GetID() string {
+	return bc.ID
+}
